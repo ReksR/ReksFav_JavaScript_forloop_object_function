4c8a2942265beb28d151d39a58092f026ad475aa
devopsrekha <61942349+devopsrekha@users.noreply.github.com>
2020-07-10 06:02:05 +0530

    pushed added push in array

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
-
 const SlamBookofReks = {
-  Name:"rekha",
-  PlaceOfBirth:"Kanyakumari",
-  FavPlaces:["Chikamagalur", "AthirapalyWatrFalls","Kuderamooka","Bangalore"],
-  FavFood:["Curd Rice with Vendaka","Spicy Biriyani","Vanila Icecream","raagi poritge","Tea"],
-  Passion:["Farming", "Be with Nature", "Live without being afraid of tomorrow","To be Myself","Buisness"],
-  FavPeople:["Strictly_No_Human,Just_God"],
-  PeopleComments : function () {
-  this.FriendsComment = []
-  this.FamilyComment=[]
-for (var i =0; i <2; i++){
-
-this.FriendsComment[i]= prompt("enter your comments about rekha")
-this.FamilyComment[i]= prompt("enter abt reks")
-}
+  Name: "rekha",
+  PlaceOfBirth: "Kanyakumari",
+  FavPlaces: [
+    "Chikamagalur",
+    "AthirapalyWatrFalls",
+    "Kuderamooka",
+    "Bangalore"
+  ],
+  FavFood: [
+    "Curd Rice with Vendaka",
+    "Spicy Biriyani",
+    "Vanila Icecream",
+    "raagi poritge",
+    "Tea"
+  ],
+  Passion: [
+    "Farming",
+    "Be with Nature",
+    "Live without being afraid of tomorrow",
+    "To be Myself",
+    "Buisness"
+  ],
+  FavPeople: ["Strictly_No_Human,Just_God"],
+  PeopleComments: function() {
+    this.FriendsComment = [];
+    this.FamilyComment = [];
+    for (var i = 0; i < 2; i++) {
+      this.FriendsComment[i] = prompt("enter about rekha");
+      this.FamilyComment[i] = this.FamilyComment.push("gil", "nice");
+    }
   }
-}
-SlamBookofReks.PeopleComments()
-console.log(SlamBookofReks)
+};
+SlamBookofReks.PeopleComments();
+console.log(SlamBookofReks);
